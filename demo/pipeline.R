ff9db04a053247da40522e14ecf48daace54d550
Sabrina <sabrina.rispin@gmail.com>
2018-02-28 16:41:02 +1100

    add pauses to demo

--- demo/pipeline.R
+++ demo/pipeline.R
@@ -50,6 +50,9 @@ scale <- 500
 rest_sec <- 180
 
 
+readline("press any key to continue")
+
+
 ##################
 # load tcx files #
 ##################
@@ -66,16 +69,21 @@ energy_df <- load_tcx_file(single_tcx_file_path)
 energy_dfs <- load_tcx_dir(directory_tcx_files, recursive = TRUE)
 str(energy_dfs)
 
+readline("press any key to continue")
+
 # select subset of energy_dfs
 # use a regex on the energy_dfs names to select certain loaded energy dataframes
 names(energy_dfs)  # all energy dataframes
+readline("press any key to continue")
 jogging <- select_list(energy_dfs, "Person1.*Jog")
 names(jogging)     # only jogging dataframes
+readline("press any key to continue")
 
 # merge multiple energy dataframes into one
 # NOTE: possible because they share the same column names
 jogging_merged <- merge_list(jogging)
 str(jogging_merged)
+readline("press any key to continue")
 
 
 #################
@@ -90,10 +98,12 @@ single_profile <- get_profiles(energy_list, buffer_start, buffer_end,
                                scale, rest_sec = rest_sec,
                                norm = FALSE, all_activities = FALSE,
                                time_threshold = 15, rest_threshold = 5)
+readline("press any key to continue")
 
 # get_profiles returns a list, to extract the first one do this:
 single_profile <- single_profile[[1]]
 str(single_profile)
+readline("press any key to continue")
 
 # get a list of profiles from a list of energy dataframes
 # SIDE NOTE:
@@ -107,12 +117,15 @@ many_profiles <- get_profiles(energy_dfs, buffer_start, buffer_end,
 
 # NOTE some energy dfs end contain multiple activities
 str(many_profiles)  # all extracted profiles
+readline("press any key to continue")
 
 # example of energy df with multiple activities
 names(select_list(many_profiles, "Person2/Run2/Sprint"))
+readline("press any key to continue")
 
 # example of energy df with a single activity
 names(select_list(many_profiles, "Person1/Run1/Walk"))
+readline("press any key to continue")
 
 
 # get a list of profiles normalised between 0 and 1
@@ -122,6 +135,7 @@ many_norm_profiles <- get_profiles(energy_dfs, buffer_start, buffer_end,
                                    time_threshold = 15, rest_threshold = 5)
 
 plot_profiles(many_norm_profiles, names(many_norm_profiles))
+readline("press any key to continue")
 
 
 ##########################
@@ -136,7 +150,9 @@ agg_profile1 <- aggregate_profiles(person1)
 agg_profile2 <- aggregate_profiles(person2)
 
 str(person1)  # list of profiles
+readline("press any key to continue")
 str(agg_profile1)  # their aggregate
+readline("press any key to continue")
 
 
 #################
@@ -146,39 +162,50 @@ str(agg_profile1)  # their aggregate
 # Plot a single profile
 plot_profile(single_profile, "Stair Climb", sections = "shade",
              title = "Stair Climb (shaded sections)")
+readline("press any key to continue")
 
 # use lines instead of shading
 plot_profile(single_profile, "Stair Climb", sections = "lines",
              title = "Stair Climb (line divided sections)")
+readline("press any key to continue")
 
 # no shading, or add shading manually
 p <- plot_profile(single_profile, "Stair Climb", sections = "none",
                   title = "Stair Climb (line divided sections)")
 p
+readline("press any key to continue")
+
 p <- add_sections(p, scale = scale, shade = TRUE, opacity = 0.4)  # add shade
 p
+readline("press any key to continue")
+
 p <- add_sections(p, scale = scale, shade = FALSE)  # add lines
 p
+readline("press any key to continue")
 
 # Plot many profiles
 subset <- select_list(many_profiles, "Person1")
 plot_profiles(subset, names(subset), sections = "shade",
               title = "Many Profiles")
+readline("press any key to continue")
 
 # Plot an aggregate profile
 p <- plot_profile(agg_profile1, "Person1", sections = "lines",
                   title = "Person1 Aggregate")
 p
+readline("press any key to continue")
 
 # you can also add another aggregate to the plot manually, simply pass in p
 plot_profile(agg_profile2, "Person2", sections = "none",
              title = "Person1 Aggregate", p = p, color = "orange")
+readline("press any key to continue")
 
 # Plot many aggregate profiles - the better way
 many_agg_profiles <- list(agg_profile1, agg_profile2)  # list them
 labels <- c("Person1", "Person2")  # give them meaningful labels
 plot_profiles(many_agg_profiles, labels, sections = "none",
               title = "Agg of Person 1 and 2")
+readline("press any key to continue")
 
 
 ###################
@@ -188,14 +215,17 @@ plot_profiles(many_agg_profiles, labels, sections = "none",
 # Plot heart rate during activities
 p <- plot_active(energy_df, type = "bpm")
 p
+readline("press any key to continue")
 
 # Add energy expenditure (pass in plot p)
 plot_active(energy_df, p = p, type = "totalE")
+readline("press any key to continue")
 
 # Merge multiple data frames to plot them
 merged_person1_run1 <- merge_list(select_list(energy_dfs, "Person1/Run1"))
 plot_active(merged_person1_run1, real_time = TRUE,
             rest_threshold = 5, time_threshold = 15)
+readline("press any key to continue")
 
 # plot profiles in 3d based on energy
 person1 <- select_list(energy_dfs, "Person1.*/(Walk|Sprint)")
@@ -205,17 +235,21 @@ person2 <- select_list(energy_dfs, "Person2")
 # plot energies
 plot_3d_profiles(person1, buffer_start, buffer_end, scale,
                  time_threshold = 30, plot_energy = F)
+readline("press any key to continue")
 
 # plot profiles
 plot_3d_landscape(person1, buffer_start, buffer_end, scale,
                   time_threshold = 30)
+readline("press any key to continue")
 
 # plot profile landscape
 plot_3d_landscape(person2, buffer_start, buffer_end, scale,
                   time_threshold = 30)
+readline("press any key to continue")
 
 plot_3d_landscape(energy_dfs, buffer_start, buffer_end, scale,
                   time_threshold = 30)
+readline("press any key to continue")
 
 
 
@@ -228,18 +262,21 @@ plot_3d_landscape(energy_dfs, buffer_start, buffer_end, scale,
 single_features <- get_features(list("prof" = single_profile), buffer_end, rest_sec,
                                 degree_coef_active = 5, degree_coef_init_rec = 5)
 head(single_features)
+readline("press any key to continue")
 
 # get features for many profiles
 # NOTE: many_profiles is already a named list
 many_features <- get_features(many_profiles, buffer_end, rest_sec,
                   degree_coef_active = 5, degree_coef_init_rec = 5)
 head(many_features)
+readline("press any key to continue")
 
 
 # get features from an aggregate profile
 agg_profile1_features <- get_features(list("agg1" = agg_profile1), buffer_end, rest_sec,
                                       degree_coef_active = 5, degree_coef_init_rec = 5)
 head(agg_profile1_features)
+readline("press any key to continue")
 
 
 # get features from multiple aggregate profiles
@@ -247,6 +284,7 @@ agg_profiles <- list("agg1" = agg_profile1, "agg2" = agg_profile2)
 agg_profiles_features <- get_features(agg_profiles, buffer_end, rest_sec,
                       degree_coef_active = 5, degree_coef_init_rec = 5)
 head(agg_profiles_features)
+readline("press any key to continue")
 
 
 ##############################
@@ -255,21 +293,26 @@ head(agg_profiles_features)
 
 # plot features of single profile
 plot_features_profile(single_profile, buffer_end)
+readline("press any key to continue")
 
 # plot them on the profile
 p <- plot_profile(single_profile, "Person 1", title = "Person 1 Features", sections = "none")
 plot_features_profile(single_profile, buffer_end, p)
+readline("press any key to continue")
 
 # plot features on top of aggregate profile
 p <- plot_profile(agg_profile1, "Person 1", sections = "none")
 plot_features_profile(agg_profile1, buffer_end, p)
+readline("press any key to continue")
 
 # plot features of many profiles
 plot_features_profiles(many_profiles, names(many_profiles), buffer_end)
+readline("press any key to continue")
 
 # plot features of multiple aggregate profiles
 agg_profiles <- list(agg_profile1, agg_profile2)
 names <- list("Person 1", "Person 2")
 plot_features_profiles(agg_profiles, names, buffer_end)
+readline("press any key to continue")
 
 
